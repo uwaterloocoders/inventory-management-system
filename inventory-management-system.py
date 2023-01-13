3c473b07cb7805442adb6f8d4d25b66a545bdd5f
Kushal Goel <64199835+kushalgoel786@users.noreply.github.com>
2023-01-12 20:53:58 -0500

    Add Starter Database Creation Code

--- inventory-management-system.py
+++ inventory-management-system.py
@@ -1,1 +1,17 @@
+from tabulate import tabulate
+import mysql.connector
+mydb=mysql.connector.connect(host="localhost", 
+                           user="root", 
+                           passwd=" ")
+cur=mydb.cursor()
 
+cur.execute("Create database Shop_Management")
+cur.execute("show databases")
+for i in cur:
+    print(i)
+
+cur.execute('create table SHOP( ItemNo integer(3), ItemName varchar(30), Quantity integer(5), Price integer(5))')
+
+cur.execute("show tables")
+for i in cur:
+    print(i)
