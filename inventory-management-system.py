f449bd7a0028c8531588778ccc13bb1b0e40b270
Aadhyaaa Mashru <97175494+aadhyaaamashru@users.noreply.github.com>
2023-01-12 21:13:06 -0500

    Add Function for Adding a Record

--- inventory-management-system.py
+++ inventory-management-system.py
@@ -15,3 +15,26 @@ cur.execute('create table SHOP( ItemNo integer(3), ItemName varchar(30), Quantit
 cur.execute("show tables")
 for i in cur:
     print(i)
+
+# ***** FUNCTIONS FOR INVENTORY ***** #
+# Function for Adding Records
+
+def additem():
+    while True:
+        print("\nContents in table SHOP")
+        a=cur.execute("select * from Shop")
+        cur.execute(a)
+        print(tabulate(cur, headers =['ItemNo', 'ItemName', 'Quantity','Price'],tablefmt='psql'))
+    
+        print("\nADDING RECORDS ...")
+        itno=int(input("\nEnter Item Number - "))
+        itnm=input("Enter Item Name - ")
+        qty=int(input("Enter Quantity - ")) 
+        pr=int(input("Enter price - Rs. "))
+        cur.execute("insert into shop values( {},'{}',{},{})".format(itno,itnm,qty,pr))
+        mydb.commit()
+        print("\nRecord Added Successfully !!!")
+
+        abc=input("\nDo you want to Add more Records ? (y/n) - ")
+        if abc=='n':
+            break
